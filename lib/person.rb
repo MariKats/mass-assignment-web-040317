46c547b264e51bfdd80200dda3d2efa3b6dae454
github username <github email address>
2017-04-15 16:17:43 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  # .send() is another way to call a method on an object. The method called is the setter method #{attribute}= and the object is the instance of our class.  
+
+  def initialize(attributes_hash)
+    attributes_hash.each {|attribute, value| self.send(("#{attribute}="), value)}
+  end
+
 end
